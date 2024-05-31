6472eb83048797313d1f7a8d67834d2afaed9af4
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-05-31 14:21:07 +0100

    docs: added docstrings to apiHndler

--- src/modules/apiHandler.py
+++ src/modules/apiHandler.py
@@ -3,20 +3,23 @@ from github.Organization import Organization
 from github.Repository import Repository
 from github.ContentFile import ContentFile
 
-def initialise_api(token:str, org_username:str, rep_name:str) -> tuple[Github | None, Organization | None, Repository | None] | None:
+def initialise_api(token:str, org_name:str, repo_name:str) -> tuple[Github | None, Organization | None, Repository | None] | None:
+  """Initialises GitHub API and returns instances of GitHub, Organisation and Repository."""
   g = get_github_instance(token)
   if g == None:
     return None
-  org = get_organisation(g, org_username)
+  org = get_organisation(g, org_name)
   if org == None:
     return None
   # Check .github repo
-  repo = get_repo(org, 'testing-repo')
+  repo = get_repo(org, repo_name)
   if repo == None:
     return None
 
-  return (g, org, repo)
+  return g, org, repo
+
 def get_github_instance(token: str) -> Github | None:
+  """Returns a GitHub instance if token is valid, else None."""
   auth = Auth.Token(token)
   g = Github(auth=auth)
   try:
@@ -26,15 +29,17 @@ def get_github_instance(token: str) -> Github | None:
     print(f"Failed to authenticate: {e.data['message']}")
     return None
 
-def get_organisation(g: Github, username: str) -> Organization | None:
+def get_organisation(g: Github, org_name: str) -> Organization | None:
+  """Returns a Organisation instance if org_name is valid, else None."""
   try:
-    org = g.get_organization(username)
+    org = g.get_organization(org_name)
     return org
   except GithubException as e:
     print(f"Failed to find organisation: {e.data['message']}")
     return None
 
 def get_repo(org: Organization, repo_name: str) -> Repository | None:
+  """Returns a Repository instance if repo_name is valid, else None."""
   try:
     repo = org.get_repo(repo_name)
     return repo
@@ -43,6 +48,7 @@ def get_repo(org: Organization, repo_name: str) -> Repository | None:
     return None
   
 def get_file(r: Repository, file_name: str) -> list[ContentFile] | ContentFile | None:
+  """Returns a ContentFile instance if file_name is valid, else None."""
   try:
     file = r.get_contents(file_name)
     return file
