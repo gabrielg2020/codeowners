7dec7a45417979b8449c838ebbc84d997885cece
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-06-03 17:41:34 +0100

    fix(apiHandler): getting get_codeowners_history_file ready for integration testing

--- src/modules/apiHandler/apiHandler.py
+++ src/modules/apiHandler/apiHandler.py
@@ -19,10 +19,10 @@ def initialise_api(token:str, org_name:str, repo_name:str) -> tuple[Github | Non
 
   return g, org, repo
 
-def get_codeowners_history_file(r: Repository) -> dict | None:
+def get_codeowners_history_file(r: Repository, file_name: str = 'co_history.json') -> dict | None:
   """Returns the contents of the co_history.json file"""
   try:
-    file_contents = get_file(r, 'co_history.json')
+    file_contents = get_file(r, file_name)
     if file_contents is None:
       # Create codeowners_history.json
       pass
