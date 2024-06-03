493d753896bf4c2d1ab9c02bb133147f7ebcc53e
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-06-03 16:07:55 +0100

    chore: add methods to init file

--- src/modules/shuffler/__init__.py
+++ src/modules/shuffler/__init__.py
@@ -1,3 +1,3 @@
-from .shuffler import shuffle_members_current_repos
+from .shuffler import shuffle_members_current_repos, add_members_to_developers, get_new_repo
 
-__all__ = ['shuffle_members_current_repos']
+__all__ = ['shuffle_members_current_repos', 'add_members_to_developers', 'get_new_repo']
