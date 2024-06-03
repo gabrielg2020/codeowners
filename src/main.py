d214c1e9b8e1a1a7ff833fbf38335602d7edac6b
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-06-03 13:53:41 +0100

    feat: add members to co_history that didnt exist

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main() -> None:
   repos = get_repos(org)
   co_history = get_codeowners_history_file(repo)
 
-  print(shuffle_members(members, repos))
+  print(shuffle_members(members, repos, co_history))
 
 if __name__ == "__main__":
   main()
--- src/modules/shuffler/shuffler.py
+++ src/modules/shuffler/shuffler.py
@@ -1,18 +1,18 @@
 import random
 
-def shuffle_members(members: list, repos: list) -> dict[str, list]:
-  co_history = {"developers":[]}
+def shuffle_members(members: list, repos: list, co_history: dict) -> dict[str, list]:
+  developers = co_history["developers"]
+  # Add members that don't exist in developers
   for member in members:
-    # Pick repo
-    repo = random.choice(repos)
-    repos.remove(repo)
-    # Create json
-    developer = {
-      "acc_name": member,
-      "number_of_times_co": 1,
-      "repos": [repo]
-    }
-    co_history["developers"].append(developer)
+    if any(developer['acc_name'] == member for developer in developers):
+        print(f"{member} exists in developers.")
+    else:
+      print(f'{member} doesnt exist in dev')
+      developers.append({
+        "acc_name": member,
+        "number_of_times_co": 0,
+        "repos": []
+      })
 
-  return co_history
+  return developers
 
