15161734f7184b81f29d18ce1f3d4ede43a3122f
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-06-06 03:07:46 +0100

    refactor(test): refactored get_repo to be reusable

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,10 +1,12 @@
 import os
 from github import Github
 from github.Organization import Organization
+from github.Repository import Repository
 import pytest
 from modules.apiHandler import (
   get_github_instance,
-  get_organisation
+  get_organisation,
+  get_repo
 )
 
 # --- get_github_instance ---
@@ -39,14 +41,43 @@ def invalid_organisation(valid_github) -> Organization | None:
 
 @pytest.fixture
 def invalid_organisation_invalid_github_valid_name(invalid_github) -> Organization | None:
-  """Fixture to provide an invalid Organisation with invalid github"""
+  """Fixture to provide an invalid Organisation with invalid Github"""
   valid_org_name = 'codeowners-rfc-test'
   org = get_organisation(invalid_github, valid_org_name)
   return org
 
 @pytest.fixture
 def invalid_organisation_invalid_github_invalid_name(invalid_github) -> Organization | None:
-  """Fixture to provide an invalid Organisation with invalid github & invalid name"""
+  """Fixture to provide an invalid Organisation with invalid Github & invalid name"""
   invalid_org_name = 'invalid_org_name'
   org = get_organisation(invalid_github, invalid_org_name)
   return org
+
+# --- get_repo ---
+@pytest.fixture
+def valid_repo(valid_organisation)  -> Repository | None:
+  """Fixture to provide a valid Repository"""
+  valid_repo_name = 'testing-repo'
+  repo = get_repo(valid_organisation, valid_repo_name)
+  return repo
+
+@pytest.fixture
+def invalid_repo(valid_organisation)  -> Repository | None:
+  """Fixture to provide an invalid Repository with invalid name"""
+  invalid_repo_name = 'invalid_repo_name'
+  repo = get_repo(valid_organisation, invalid_repo_name)
+  return repo
+
+@pytest.fixture
+def invalid_repo_invalid_org_valid_name(invalid_organisation)  -> Repository | None:
+  """Fixture to provide an invalid Repository with invalid Organisation """
+  valid_repo_name = 'testing-repo'
+  repo = get_repo(invalid_organisation, valid_repo_name)
+  return repo
+
+@pytest.fixture
+def invalid_repo_invalid_org_invalid_name(invalid_organisation)  -> Repository | None:
+  """Fixture to provide an invalid Repository with invalid Organisation & invalid name"""
+  invalid_repo_name = 'invalid_repo_name'
+  repo = get_repo(invalid_organisation, invalid_repo_name)
+  return repo
--- tests/unit/test_apiHandler.py
+++ tests/unit/test_apiHandler.py
@@ -18,21 +18,38 @@ def test_get_github_instance_invalid_token(invalid_github) -> None:
   assert invalid_github is None
 
 # --- get_organisation ---
-def test_get_organisation_valid_name(valid_organisation) -> None:
-  """Test organisation grabbing with valid github & valid organisation name"""
+def test_get_organisation_valid_github_valid_name(valid_organisation) -> None:
+  """Test organisation grabbing with valid Github & valid organisation name"""
   assert valid_organisation is not None
 
 def test_get_organisation_valid_github_invalid_name(invalid_organisation) -> None:
-  """Test organisation grabbing with valid github & invalid organisation name"""
+  """Test organisation grabbing with valid Github & invalid organisation name"""
   assert invalid_organisation is None
 
 def test_get_organisation_invalid_github_valid_name(invalid_organisation_invalid_github_valid_name) -> None:
-  """Test organisation grabbing with invalid github & valid organisation name"""
+  """Test organisation grabbing with invalid Github & valid organisation name"""
   assert invalid_organisation_invalid_github_valid_name is None
 
 def test_get_organisation_invalid_github_invalid_name(invalid_organisation_invalid_github_invalid_name) -> None:
-  """Test organisation grabbing with invalid github & invalid organisation name"""
+  """Test organisation grabbing with invalid Github & invalid organisation name"""
   assert invalid_organisation_invalid_github_invalid_name is None
+
+# --- get_repo ---
+def test_get_repo_valid_organisation_valid_repo_name(valid_repo) -> None:
+  """Test repo grabbing with valid organisation & valid repo name"""
+  assert valid_repo is not None
+
+def test_get_repo_valid_organisation_invalid_repo_name(invalid_repo) -> None:
+  """Test repo grabbing with valid organisation & invalid repo name"""
+  assert invalid_repo is None
+
+def test_get_repo_invalid_organisation_valid_repo_name(invalid_repo_invalid_org_valid_name) -> None:
+  """Test repo grabbing with invalid organisation & valid repo name"""
+  assert invalid_repo_invalid_org_valid_name is None
+
+def test_get_repo_invalid_organisation_invalid_repo_name(invalid_repo_invalid_org_invalid_name) -> None:
+  """Test repo grabbing with invalid organisation & invalid repo name"""
+  assert invalid_repo_invalid_org_invalid_name is None
 
 # --- get_repos ---
 def test_get_repos_valid_organisation(valid_organisation) -> None:
@@ -42,22 +59,5 @@ def test_get_repos_valid_organisation(valid_organisation) -> None:
 def test_get_repos_invalid_organisation(invalid_organisation) -> None:
   """Test repos grabbing with invalid organisation"""
   assert get_repos(invalid_organisation) is None
-
-# --- get_repo ---
-def test_get_repo_valid_organisation_valid_repo_name(valid_organisation) -> None:
-  """Test repo grabbing with valid organisation & valid repo name"""
-  assert get_repo(valid_organisation, 'testing-repo') is not None
-
-def test_get_repo_valid_organisation_invalid_repo_name(valid_organisation) -> None:
-  """Test repo grabbing with valid organisation & invalid repo name"""
-  assert get_repo(valid_organisation, 'invalid_repo_name') is None
-
-def test_get_repo_invalid_organisation_valid_repo_name(invalid_organisation) -> None:
-  """Test repo grabbing with invalid organisation & valid repo name"""
-  assert get_repo(invalid_organisation, 'testing-repo') is None
-
-def test_get_repo_invalid_organisation_invalid_repo_name(invalid_organisation) -> None:
-  """Test repo grabbing with invalid organisation & invalid repo name"""
-  assert get_repo(invalid_organisation, 'invalid_repo_name') is None
 
 
