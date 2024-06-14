573c847fc960f00c958eba5c2b68583c342d7ee1
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-06-14 13:35:24 +0100

    test: tests added for shuffle_members_current_repos

--- tests/unit/test_shuffler.py
+++ tests/unit/test_shuffler.py
@@ -3,7 +3,8 @@ from modules.shuffler import (
   add_members_to_developers,
   backtrack, 
   find_least_repeated_repo,
-  get_developer_repo_distribution
+  get_developer_repo_distribution,
+  shuffle_members_current_repos
 )
 
 def test_add_members_to_developers(valid_members, valid_unfinished_developers, valid_developers) -> None:
@@ -136,3 +137,14 @@ def test_get_developer_repo_distribution_with_all_repos_assigned() -> None:
   ]
 
   assert get_developer_repo_distribution(developers, repos) == expected_result
+
+def test_shuffle_members_current_repos(valid_members, valid_developers) -> None:
+  repos = ['repo_1', 'repo_2', 'repo_3']
+
+  expected_result = [
+    {'acc_name': 'member_2', 'number_of_times_co': 1, 'current_repo': 'repo_1', 'repos': ['repo_1']}, 
+    {'acc_name': 'member_3', 'number_of_times_co': 1, 'current_repo': 'repo_2', 'repos': ['repo_2']}, 
+    {'acc_name': 'member_1', 'number_of_times_co': 2, 'current_repo': 'repo_3', 'repos': ['repo_3', 'repo_1']}
+  ]
+
+  assert shuffle_members_current_repos(valid_members, repos, valid_developers) == expected_result
