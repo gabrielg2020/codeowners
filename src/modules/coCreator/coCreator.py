79ec4fa3a7bc39a9b9c45b0e382b936fbb7e7c1b
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-07-26 23:48:06 +0100

    fix(lint): coCreator

--- src/modules/coCreator/coCreator.py
+++ src/modules/coCreator/coCreator.py
@@ -4,6 +4,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def create_codeowners_files(developers: dict, repos: list) -> dict:
+  """Returns a repo_codeowners_map from a developer dictionary."""
   repo_developer_map = {}
   for developer in developers:
     current_repo = developer['current_repo']
@@ -11,16 +12,21 @@ def create_codeowners_files(developers: dict, repos: list) -> dict:
       repo_developer_map[current_repo].append(developer['acc_name'])
     else:
       repo_developer_map[current_repo] = [developer['acc_name']]
-  
+
   repo_codeowners_map = {}
   for repo in repos:
     codeowners_string = '# This file shows that '
-    if repo in repo_developer_map.keys():
+    if repo in repo_developer_map:
       owners = repo_developer_map[repo]
-      codeowners_string += f'{ "nobody " if owners == [] else " and ".join("@" + owner for owner in owners)} own the {repo} repository. \n * {" ".join("@" + owner for owner in owners)}'
+      if owners:
+        owners_str = " and ".join("@" + owner for owner in owners)
+        codeowners_string += f'{owners_str} own the {repo} repository. \n * {" ".join("@" + owner for owner in owners)}'
+      else:
+        codeowners_string += f'nobody owns the {repo} repository. '
     else:
       codeowners_string += f'nobody owns the {repo} repository.'
 
     repo_codeowners_map[repo] = codeowners_string
+
 
   return repo_codeowners_map
